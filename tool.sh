b4b4e479d5feafc03280fe570bc6ddbae8576934
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-04 11:32:45 -0400

    update

---
+++ tool.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#path:/home/tool.sh
+echo "hello tizania <3"
+chmod +x tool.sh # make the file executable by the user running the script with admin privileges
+
+
+mkdir style
+sleep 1
+cd style 
+touch style.css
+# echo "body {background-color: #000000;}" >> style.css
+touch script.js
+# echo "console.log('Hello World');" >> script.js
+touch style.scss
+# echo "body {background-color: #000000;}" >> style.scss
+cd ..
+mkdir views
+cd views
+touch index.html
+# echo "<!DOCTYPE html>
+# <html lang="en">
+cd ..
