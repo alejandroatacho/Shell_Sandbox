276e245ecf97beca0b5f2217c333d3fda2bf047a
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-03 22:34:22 -0400

    Create make_folder.sh

---
+++ cmd/make_folder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir test #mkdir is a command that creates a directory
+touch test/test.txt #touch is a command that creates a file
+echo test
+ls test #ls is a command that lists the contents of a directory
+sleep 2 #sleep is a command that suspends the execution of the script for a given number of seconds
+rm test/test.txt #rm is a command that removes a file
+rmdir test #rmdir is a command that removes a directory
