7a3ae483b6dde967293fb6e641da4b4a0d723f94
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-03 22:04:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #path:/home/run.sh
 # VARIABLES
+
+chmod +x run.sh # make the file executable by the user running the script with admin privileges
+
 cd cmd/
 for file in $(ls)
 do
@@ -8,5 +11,6 @@ do
     ./$file
     
     done
+cd ..
 
 $SHELL #prevent it from closing
