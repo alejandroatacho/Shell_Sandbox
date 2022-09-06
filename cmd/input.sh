bed09e5fee1ebac5dd3ab8b110f16e143ea0b81a
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-06 13:07:48 -0400

    Update input.sh

--- cmd/input.sh
+++ cmd/input.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # VARIABLES
-echo "Enter name : "
+echo "Enter name (1/2): "
+admin_name="alex"
+admin_pass="1234"
 read name 
-read name2 
-read name3
+echo "Enter password (2/2): "
+read password 
 #read name name2 name3
 echo "Enterd names : $name  $name2  $name3"
+if [ $name == $admin_name ] && [ $password == $admin_pass ]
+    then 
+    echo "Welcome $name"
+
+else
+    echo "You are not $admin_name all access has been revoked"
+fi
