2c530aae64c70efe690f081b33b399fdb0f89209
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-06 13:21:26 -0400

    Update input.sh

--- cmd/input.sh
+++ cmd/input.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # VARIABLES
-echo "Enter name (1/2): "
 admin_name="alex"
 admin_pass="1234"
+echo "Enter name (1/2): "
 read name 
 echo "Enter password (2/2): "
 read password 
 #read name name2 name3
-echo "Enterd names : $name  $name2  $name3"
+echo "Enterd names : $name"
 if [ $name == $admin_name ] && [ $password == $admin_pass ]
     then 
     echo "Welcome $name"
